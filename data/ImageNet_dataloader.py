1eedbefdff6354f5eebf6a745f9ab95b9395f76c
Gang Li <ucasligang@gmail.com>
2023-03-03 18:57:19 +0800

    Add files via upload

---
+++ data/ImageNet_dataloader.py
@@ -0,0 +1,22 @@
+from data.folder import ImageFolder
+import numpy as np
+
+class ImageNetDataSet(ImageFolder):
+    def __getitem__(self, index: int):
+        """
+        Args:
+            index (int): Index
+
+        Returns:
+            tuple: (sample, target) where target is class_index of the target class.
+        """
+        path, target = self.samples[index]
+        # sample = self.loader(path)
+        sample = np.load(path)
+
+        if self.transform is not None:
+            sample = self.transform(sample)
+        if self.target_transform is not None:
+            target = self.target_transform(target)
+        return sample, target
+
